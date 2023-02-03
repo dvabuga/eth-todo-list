5433546cf3e6d622d37abb9b9aef172af24cb76f
Denis Shebalov <v-deshebalov@microsoft.com>
2023-02-03 18:23:02 +0100

    app state after first migration to chain

---
+++ contracts/TodoList.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract TodoList
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+/// @return Documents the return variables of a contract’s function state variable
+/// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
+{
+    uint public taskCount = 0;
+}
